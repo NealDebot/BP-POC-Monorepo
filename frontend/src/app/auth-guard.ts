0d91f25941f57a0958d951efe00105a00a45fb55
NealDebot <Neal.debot@telenet.be>
2026-04-21 16:34:19 +0200

    infinite loop on firefox fix attempt 3.0

--- frontend/src/app/auth-guard.ts
+++ frontend/src/app/auth-guard.ts
@@ -1,13 +1,19 @@
 import { CanActivateFn } from '@angular/router';
 import { AuthService } from '@auth0/auth0-angular';
 import { inject } from '@angular/core';
+import {filter, take, tap, switchMap} from 'rxjs'
 
 export const authGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService)
-  authService.isAuthenticated$.subscribe(isAuthenticated=>{
-    if (!isAuthenticated){
-      authService.loginWithRedirect()
-    }
-  })
-  return authService.isAuthenticated$
+  return authService.isLoading$.pipe(
+    filter((loading) => !loading),
+    take(1),
+    switchMap(() => authService.isAuthenticated$),
+    take(1),
+    tap((isAuthenticated) => {
+      if (!isAuthenticated) {
+        authService.loginWithRedirect();
+      }
+    }),
+  );
 };
